f3b3c8095c00e2a998224949a9973f9a330c3ed7
anuj26298 <kumar26298anuj@gmail.com>
2021-05-27 17:47:13 +0530

    ADDED create board feature

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -1,5 +1,14 @@
 public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("__x-o-x-o__ Tic Tac Toe __x-o-x-o__");
+        char[] board = createBoard();
+    }
+
+    private static char[] createBoard(){
+        char[] board = new char[10];
+        for (int i=1;i< board.length; i++){
+            board[i] = ' ';
+        }
+        return board;
     }
 }

d743c11e3ad4ce46563eaa55e8b1882030e458d1
anuj26298 <kumar26298anuj@gmail.com>
2021-05-27 17:52:48 +0530

    ADDED choose user letter feature

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("__x-o-x-o__ Tic Tac Toe __x-o-x-o__");
         char[] board = createBoard();
+        Scanner userInput = new Scanner(System.in);
+
+        char userLetter = chooseUserLetter(userInput);
+        char computerLetter = (userLetter == 'X') ? 'O' : 'X';
+        System.out.println("User Letter: " + userLetter);
+        System.out.println("Computer Letter: " + computerLetter);
     }
 
     private static char[] createBoard(){
@@ -10,5 +18,9 @@ public class TicTacToeGame {
             board[i] = ' ';
         }
         return board;
+    }
+    private static char chooseUserLetter(Scanner userInput){
+        System.out.println("Choose Your Letter(X or O)");
+        return userInput.next().toUpperCase().charAt(0);
     }
 }

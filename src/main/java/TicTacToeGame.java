f1bf9e3876f858cc51ca8ac4fb2af1ed71f0bfe1
anuj26298 <kumar26298anuj@gmail.com>
2021-05-27 21:24:01 +0530

    ADDED check winning, tie and turn feature

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -71,4 +71,42 @@ public class TicTacToeGame {
             return false;
         }
     }
+
+    private boolean checkWinning(char[] board, char userLetter, char computerLetter){
+        if(checkBoard(board,userLetter)){
+            System.out.println("You Won!!!");
+            return false;
+        }
+
+        if(checkBoard(board,computerLetter)){
+            System.out.println("Computer Wins...");
+            return false;
+        }
+
+        int flag = 0;
+        for (int i = 0; i<9; i++) {
+            if (board[i] == ' ') {
+                flag = 1;
+                break;
+            }
+        }
+            if (flag == 0) {
+                System.out.println("Match tied");
+                return false;
+            }
+        return true;
+    }
+
+    private boolean checkBoard(char[] board, char letter){
+        return (board[0] == letter && board[1] == letter && board[2] == letter ) ||
+                (board[3] == letter && board[4] == letter && board[5] == letter )||
+                (board[6] == letter && board[7] == letter && board[8] == letter )||
+                (board[0] == letter && board[3] == letter && board[6] == letter )||
+                (board[1] == letter && board[4] == letter && board[7] == letter )||
+                (board[2] == letter && board[5] == letter && board[8] == letter )||
+                (board[0] == letter && board[4] == letter && board[8] == letter )||
+                (board[2] == letter && board[4] == letter && board[6] == letter );
+    }
+    
+
 }

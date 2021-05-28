124b0d726f8c44ee42db1b512859a461218ec7e7
anuj26298 <kumar26298anuj@gmail.com>
2021-05-28 14:57:45 +0530

    ADDED Continue Game feature

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -4,68 +4,71 @@ import java.util.Scanner;
 public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("__x-o-x-o__ Tic Tac Toe __x-o-x-o__");
-        char[] board = createBoard();
         Scanner userInput = new Scanner(System.in);
-        TicTacToeGame tictactoe = new TicTacToeGame();
-        char userLetter = chooseUserLetter(userInput);
-        char computerLetter = (userLetter == 'X') ? 'O' : 'X';
-        System.out.println("User Letter: " + userLetter);
-        System.out.println("Computer Letter: " + computerLetter);
+        char controlVariable;
+        do {
+            char[] board = createBoard();
+            TicTacToeGame tictactoe = new TicTacToeGame();
+            char userLetter = chooseUserLetter(userInput);
+            char computerLetter = (userLetter == 'X') ? 'O' : 'X';
+            System.out.println("User Letter: " + userLetter);
+            System.out.println("Computer Letter: " + computerLetter);
 
-        tictactoe.showBoard(board);
+            tictactoe.showBoard(board);
 
-        boolean tossResult = toss(userInput);
+            boolean tossResult = toss(userInput);
 
-        boolean play = true;
+            boolean play = true;
 
-        while (play) {
-            if (tossResult) {
-                int index = getUserMove(board, userInput);
-                tictactoe.makeMove(board, index, userLetter);
-                tictactoe.showBoard(board);
-                System.out.println("============================");
-                play = tictactoe.checkWinning(board, userLetter, computerLetter);
-                if (play) {
+            while (play) {
+                if (tossResult) {
+                    int index = getUserMove(board, userInput);
+                    tictactoe.makeMove(board, index, userLetter);
+                    tictactoe.showBoard(board);
+                    System.out.println("============================");
+                    play = tictactoe.checkWinning(board, userLetter, computerLetter);
+                    if (play) {
+                        board = tictactoe.computerInput(board, userLetter, computerLetter);
+                        tictactoe.showBoard(board);
+                        System.out.println("=============================");
+                        play = tictactoe.checkWinning(board, userLetter, computerLetter);
+                        if (!play)
+                            System.out.println("Game Over!!!");
+                    } else
+                        System.out.println("Game Over!!!");
+                } else {
                     board = tictactoe.computerInput(board, userLetter, computerLetter);
                     tictactoe.showBoard(board);
                     System.out.println("=============================");
                     play = tictactoe.checkWinning(board, userLetter, computerLetter);
-                    if (!play)
-                        System.out.println("Game Over!!!");
-                } else
-                    System.out.println("Game Over!!!");
-            }
-            else {
-                board = tictactoe.computerInput(board,userLetter,computerLetter);
-                tictactoe.showBoard(board);
-                System.out.println("=============================");
-                play = tictactoe.checkWinning(board, userLetter, computerLetter);
 
-                if (play){
-                    int index = tictactoe.getUserMove(board, userInput);
-                    tictactoe.makeMove(board, index, userLetter);
-                    tictactoe.showBoard(board);
-                    System.out.println("=============================");
-                    play = tictactoe.checkWinning(board, userLetter, computerLetter);
-                    if (!play)
+                    if (play) {
+                        int index = tictactoe.getUserMove(board, userInput);
+                        tictactoe.makeMove(board, index, userLetter);
+                        tictactoe.showBoard(board);
+                        System.out.println("=============================");
+                        play = tictactoe.checkWinning(board, userLetter, computerLetter);
+                        if (!play)
+                            System.out.println("Game Over!!!");
+                    } else
                         System.out.println("Game Over!!!");
                 }
-                else
-                    System.out.println("Game Over!!!");
             }
-        }
+            System.out.println("Do you want to play again?(y/n)");
+            controlVariable = userInput.next().charAt(0);
+        }while (controlVariable == 'Y' || controlVariable == 'y');
     }
 
 
-
-    private static char[] createBoard(){
+    private static char[] createBoard() {
         char[] board = new char[10];
-        for (int i=1;i< board.length; i++){
+        for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
         return board;
     }
-    private static char chooseUserLetter(Scanner userInput){
+
+    private static char chooseUserLetter(Scanner userInput) {
         System.out.println("Choose Your Letter(X or O)");
         return userInput.next().toUpperCase().charAt(0);
     }
@@ -79,8 +82,8 @@ public class TicTacToeGame {
                 board[8] + " | " + board[9] + " | ");
     }
 
-    private static int getUserMove(char[] board, Scanner userInput){
-        Integer[] validCells = {1,2,3,4,5,6,7,8,9};
+    private static int getUserMove(char[] board, Scanner userInput) {
+        Integer[] validCells = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         while (true) {
             System.out.println("Enter your next move(1-9)");
             int index = userInput.nextInt();
@@ -88,11 +91,12 @@ public class TicTacToeGame {
                 return index;
         }
     }
-    private static boolean isSpaceFree(char[] board, int index){
+
+    private static boolean isSpaceFree(char[] board, int index) {
         return board[index] == ' ';
     }
 
-    private static void makeMove(char[] board, int index, char letter){
+    private static void makeMove(char[] board, int index, char letter) {
         boolean spaceFree = isSpaceFree(board, index);
         if (spaceFree)
             board[index] = letter;
@@ -113,99 +117,100 @@ public class TicTacToeGame {
         }
     }
 
-    private boolean checkWinning(char[] board, char userLetter, char computerLetter){
-        if(checkBoard(board,userLetter)){
+    private boolean checkWinning(char[] board, char userLetter, char computerLetter) {
+        if (checkBoard(board, userLetter)) {
             System.out.println("You Won!!!");
             return false;
         }
 
-        if(checkBoard(board,computerLetter)){
+        if (checkBoard(board, computerLetter)) {
             System.out.println("Computer Wins...");
             return false;
         }
 
         int flag = 0;
-        for (int i = 0; i<9; i++) {
+        for (int i = 0; i < 9; i++) {
             if (board[i] == ' ') {
                 flag = 1;
                 break;
             }
         }
-            if (flag == 0) {
-                System.out.println("Match tied");
-                return false;
-            }
+        if (flag == 0) {
+            System.out.println("Match tied");
+            return false;
+        }
         return true;
     }
 
-    private boolean checkBoard(char[] board, char letter){
-        return (board[0] == letter && board[1] == letter && board[2] == letter ) ||
-                (board[3] == letter && board[4] == letter && board[5] == letter )||
-                (board[6] == letter && board[7] == letter && board[8] == letter )||
-                (board[0] == letter && board[3] == letter && board[6] == letter )||
-                (board[1] == letter && board[4] == letter && board[7] == letter )||
-                (board[2] == letter && board[5] == letter && board[8] == letter )||
-                (board[0] == letter && board[4] == letter && board[8] == letter )||
-                (board[2] == letter && board[4] == letter && board[6] == letter );
-    }
-    private boolean checkWinning(char[] tempBoard, char letter){
-        return checkBoard(tempBoard,letter);
+    private boolean checkBoard(char[] board, char letter) {
+        return (board[0] == letter && board[1] == letter && board[2] == letter) ||
+                (board[3] == letter && board[4] == letter && board[5] == letter) ||
+                (board[6] == letter && board[7] == letter && board[8] == letter) ||
+                (board[0] == letter && board[3] == letter && board[6] == letter) ||
+                (board[1] == letter && board[4] == letter && board[7] == letter) ||
+                (board[2] == letter && board[5] == letter && board[8] == letter) ||
+                (board[0] == letter && board[4] == letter && board[8] == letter) ||
+                (board[2] == letter && board[4] == letter && board[6] == letter);
     }
 
-    private boolean winMove(int index, char[] board, char letter){
+    private boolean checkWinning(char[] tempBoard, char letter) {
+        return checkBoard(tempBoard, letter);
+    }
+
+    private boolean winMove(int index, char[] board, char letter) {
         char[] tempBoard;
         tempBoard = board.clone();
         tempBoard[index] = letter;
-        return checkWinning(tempBoard,letter);
+        return checkWinning(tempBoard, letter);
     }
 
-    private char[] computerInput(char[] board, char userLetter, char computerLetter){
-        for (int i=0;i<9;i++){
-            if (board[i] == ' ' && winMove(i,board, computerLetter)){
+    private char[] computerInput(char[] board, char userLetter, char computerLetter) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i] == ' ' && winMove(i, board, computerLetter)) {
                 board[i] = computerLetter;
                 return board;
             }
         }
 
-        for (int i=0;i<9;i++){
-            if (board[i] == ' ' && winMove(i,board,userLetter)){
+        for (int i = 0; i < 9; i++) {
+            if (board[i] == ' ' && winMove(i, board, userLetter)) {
                 board[i] = computerLetter;
                 return board;
             }
         }
-        if (board[0] == ' '){
+        if (board[0] == ' ') {
             board[0] = computerLetter;
             return board;
         }
-        if (board[2] == ' '){
+        if (board[2] == ' ') {
             board[2] = computerLetter;
             return board;
         }
-        if (board[6] == ' '){
+        if (board[6] == ' ') {
             board[6] = computerLetter;
             return board;
         }
-        if (board[8] == ' '){
+        if (board[8] == ' ') {
             board[8] = computerLetter;
             return board;
         }
-        if (board[4] == ' '){
+        if (board[4] == ' ') {
             board[4] = computerLetter;
             return board;
         }
-        if (board[1] == ' '){
+        if (board[1] == ' ') {
             board[1] = computerLetter;
             return board;
         }
-        if (board[3] == ' '){
+        if (board[3] == ' ') {
             board[3] = computerLetter;
             return board;
         }
-        if (board[5] == ' '){
+        if (board[5] == ' ') {
             board[5] = computerLetter;
             return board;
         }
-        if (board[7] == ' '){
+        if (board[7] == ' ') {
             board[7] = computerLetter;
             return board;
         }
